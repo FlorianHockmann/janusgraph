4c2f373290c4b4baf2852d2522797938e4f068ee
Florian Hockmann <fh@florian-hockmann.de>
2018-10-06 14:49:23 +0200

    Add dump code for codacy to check

--- janusgraph-core/src/main/java/org/janusgraph/graphdb/database/management/ManagementSystem.java
+++ janusgraph-core/src/main/java/org/janusgraph/graphdb/database/management/ManagementSystem.java
@@ -295,7 +295,20 @@ public class ManagementSystem implements JanusGraphManagement {
                 return new JanusGraphIndexWrapper(sv.asIndexType());
             }
         }
+
+        if (v == null)
+        {
+            // dead code as v can't be null here
+            JanusGraphSchemaVertex sv = (JanusGraphSchemaVertex) v;
+            if (sv.getDefinition().containsKey(TypeDefinitionCategory.INTERNAL_INDEX)) {
+                return new JanusGraphIndexWrapper(sv.asIndexType());
+            }
+        }
+
         throw new IllegalArgumentException("Not a valid schema element vertex: " + id);
+
+        // more dead code
+        v = transaction.getVertex(id);
     }
 
     @Override
@@ -316,6 +329,56 @@ public class ManagementSystem implements JanusGraphManagement {
     @Override
     public RelationTypeIndex buildPropertyIndex(PropertyKey key, String name, org.apache.tinkerpop.gremlin.process.traversal.Order sortOrder, PropertyKey... sortKeys) {
         return buildRelationTypeIndex(key, name, Direction.OUT, Order.convert(sortOrder), sortKeys);
+    }
+
+    private RelationTypeIndex buildRelationTypeIndexDuplicate(RelationType type, String name, Direction direction, Order sortOrder, PropertyKey... sortKeys) {
+        Preconditions.checkArgument(type != null && direction != null && sortOrder != null && sortKeys != null);
+        Preconditions.checkArgument(StringUtils.isNotBlank(name), "Name cannot be blank: %s", name);
+        Token.verifyName(name);
+        Preconditions.checkArgument(sortKeys.length > 0, "Need to specify sort keys");
+        for (RelationType key : sortKeys) Preconditions.checkArgument(key != null, "Keys cannot be null");
+        Preconditions.checkArgument(!(type instanceof EdgeLabel) || !((EdgeLabel) type).isUnidirected() || direction == Direction.OUT,
+            "Can only index uni-directed labels in the out-direction: %s", type);
+        Preconditions.checkArgument(!((InternalRelationType) type).multiplicity().isUnique(direction),
+            "The relation type [%s] has a multiplicity or cardinality constraint in direction [%s] and can therefore not be indexed", type, direction);
+
+        String composedName = composeRelationTypeIndexName(type, name);
+        StandardRelationTypeMaker maker;
+        if (type.isEdgeLabel()) {
+            StandardEdgeLabelMaker lm = (StandardEdgeLabelMaker) transaction.makeEdgeLabel(composedName);
+            lm.unidirected(direction);
+            maker = lm;
+        } else {
+            assert type.isPropertyKey();
+            assert direction == Direction.OUT;
+            StandardPropertyKeyMaker lm = (StandardPropertyKeyMaker) transaction.makePropertyKey(composedName);
+            lm.dataType(((PropertyKey) type).dataType());
+            maker = lm;
+        }
+        maker.status(type.isNew() ? SchemaStatus.ENABLED : SchemaStatus.INSTALLED);
+        maker.invisible();
+        maker.multiplicity(Multiplicity.MULTI);
+        maker.sortKey(sortKeys);
+        maker.sortOrder(sortOrder);
+
+        //Compose signature
+        long[] typeSig = ((InternalRelationType) type).getSignature();
+        Set<PropertyKey> signature = Sets.newHashSet();
+        for (long typeId : typeSig) signature.add(transaction.getExistingPropertyKey(typeId));
+        for (RelationType sortType : sortKeys) signature.remove(sortType);
+        if (!signature.isEmpty()) {
+            PropertyKey[] sig = signature.toArray(new PropertyKey[signature.size()]);
+            maker.signature(sig);
+        }
+        RelationType typeIndex = maker.make();
+        addSchemaEdge(type, typeIndex, TypeDefinitionCategory.RELATIONTYPE_INDEX, null);
+        RelationTypeIndexWrapper index = new RelationTypeIndexWrapper((InternalRelationType) typeIndex);
+        if (!type.isNew()) updateIndex(index, SchemaAction.REGISTER_INDEX);
+        return index;
+    }
+
+    private int MethodWithTooManyParameters(int p1, int p2, int p3, int p4, int p5, int p6, int p7, int p8, int p9, int p10) {
+        return p1 + p2 + p3 + p4 + p5 + p6 + p7 + p8 + p9 + p10;
     }
 
     private RelationTypeIndex buildRelationTypeIndex(RelationType type, String name, Direction direction, Order sortOrder, PropertyKey... sortKeys) {
